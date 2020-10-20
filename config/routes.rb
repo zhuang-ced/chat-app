b157f322b9fb138bb656f5ab7249991013325b3f
zhuang-ced <sho.sc@icloud.com>
2020-10-20 22:10:17 +0900

    メッセージ送信機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'messages/index'
-root to: "messages#index"
- resources :users, only: [:edit, :update]
- resources :rooms, only: [:new, :create]
+  root to: "rooms#index"
+  resources :users, only: [:edit, :update]
+  resources :rooms, only: [:new, :create] do
+    resources :messages, only: [:index, :create]
+  end
 end
